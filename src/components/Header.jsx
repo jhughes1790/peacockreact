b9acb8c8fa81a56c4808d98f9dcf79ba03c4d49b
JamesHughes <jhughes1790@gmail.com>
2022-05-26 10:57:24 -0400

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Header() {
+    return (
+        <h1>Hello Mr. World </h1>
+    )
+};
+
+
+
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Main() {
+    return (
+        <h1> Mr. Hughes, Welcome to Main</h1>
+    )
+}
+
+
+export default Main;
